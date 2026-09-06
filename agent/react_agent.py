6dc63983193d697c647e6d93d39f99a9f0e75161
yydxs <657553910@qq.com>
2026-09-06 19:58:08 +0800

    第四次提交

--- agent/react_agent.py
+++ agent/react_agent.py
@@ -33,5 +33,5 @@ class ReactAgent:
 if __name__ == '__main__':
     agent = ReactAgent()
 
-    for chunk in agent.execute_stream("给我生成我的使用报告"):
-        print(chunk, end="", flush=True)
+    # for chunk in agent.execute_stream("给我生成我的使用报告"):
+    #     print(chunk, end="", flush=True)
